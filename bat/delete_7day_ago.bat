678483ef87ff8466d4776c3ba1696e1efd213d18
Jimmy <caoqiguang@gmail.com>
2020-04-15 16:23:19 +0800

    bat

--- bat/delete_7day_ago.bat
+++ bat/delete_7day_ago.bat
@@ -1,9 +1,18 @@
 @echo off
 Color a9
-echo.               ==========删除制定目录7天之前的文件==========
-echo.               =====BY:JIMMY caoqiguang@gmail.com===========
-forfiles /p "D:\jimmy\oabak" /s /m *.* /d -7 /c "cmd /c del @path"
-forfiles /p "D:\jimmy\erpbak\002" /s /m *.* /d -7 /c "cmd /c del @path"
-forfiles /p "D:\jimmy\erpbak\第二账套" /s /m *.* /d -7 /c "cmd /c del @path"
-forfiles /p "D:\jimmy\haiyibak\DB" /s /m *.* /d -7 /c "cmd /c del @path"
-@exit
+echo.               ==========将备份文件N天之前的文件自动删除,并最少保留天的备份=====
+echo.               ============BY:JIMMY caoqiguang@gmail.com==============
+echo.
+rem 初始计数器
+set cnt=0
+rem 设置将压缩文件放置在何处
+set FileDir=F:\Software
+rem 删除多少天之前的文件
+set DelDate=7
+rem 最少保留多少个文件，防止备份计划出问题时，把所有备份删光
+set LeastDate=2
+for /f "delims=" %%i in ('dir/b/a-d "%FileDir%" 2^>nul') do (set /a cnt+=1
+echo %%i
+)
+if %cnt% GTR %LeastDate% (forfiles /p "%FileDir%" /s /m *.* /d -7 /c "cmd /c del @path") else echo,文件数低于最低保留值%LeastDate%，不执行任何操作。
+pause
